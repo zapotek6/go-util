c98c4163f12256ede5561c7b08a8ff050e171369
zap <zapotek6@gmail.com>
2020-04-06 22:50:49 +0200

    add helper in getting env variables

--- util/util.go
+++ util/util.go
@@ -4,17 +4,26 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+	"time"
 )
 
 var Run = true
 
 func SetupCloseHandler() {
+	SetupCloseHandlerWFunc(nil)
+}
+
+func SetupCloseHandlerWFunc(f func()) {
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
 		fmt.Println("Ctrl+C pressed in Terminal")
+		if nil != f {
+			f()
+		}
 		Run = false
 	}()
 }
@@ -27,4 +36,42 @@ func GetEnv(varName string, defaultValue string) string {
 	}
 
 	return variable
+}
+
+func GetEnvAsInt(varName string, defaultValue int) (int, error) {
+	variable := os.Getenv(varName)
+
+	var value int
+
+	if variable == "" {
+		value = defaultValue
+	} else {
+		if val, err := strconv.ParseInt(variable, 10, 64); nil == err {
+			return int(val), nil
+		} else {
+			return -1, err
+		}
+	}
+
+	return value, nil
+}
+
+func GetEnvAsDuration(varName string, defaultValue time.Duration) (time.Duration, error) {
+	variable := os.Getenv(varName)
+
+	if variable == "" {
+		return defaultValue, nil
+	} else {
+		return time.ParseDuration(variable)
+	}
+}
+
+func GetEnvAsBool(varName string, defaultValue bool) (bool, error) {
+	variable := os.Getenv(varName)
+
+	if variable == "" {
+		return defaultValue, nil
+	} else {
+		return strconv.ParseBool(variable)
+	}
 }
